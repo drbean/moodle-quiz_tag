bf9d2e8a111b2a87f91dcc7ac4c0757606063ebb
Dr Bean <drbean@sac.nuu.edu.tw>
2019-09-01 15:19:25 +0800

    tags_in_tagcollection:1_address,but_questions_in_1_address_question_category_not_finding_them!

---
+++ lang/en/quiz_tag.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     quiz_tag
+ * @category    string
+ * @copyright   2019 Dr Bean <drbean@cpan.org>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['tagarea_question'] = 'question';
+$string['tagcollection_1_address'] = 'Week 1 tags for address';
+
+$string['pluginname'] = 'tag report';
+$string['reporttag'] = 'tag report';
+$string['tagreport'] = 'tag report';
+$string['tag'] = 'tag results';

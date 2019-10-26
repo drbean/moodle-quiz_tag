0d9c89c0e5549f8e5b8c3c734e6a9c18ff85c6cd
Dr Bean <drbean@sac.nuu.edu.tw>
2019-10-26 22:03:47 +0800

    plugin.version=s/2019090103/2019102600/

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'quiz_tag';
 $plugin->release = '0.1.0';
-$plugin->version = 2019090103;
+$plugin->version = 2019102600;
 $plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_ALPHA;
